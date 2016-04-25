fd227b93c8a1bab7b6a58764c69a856b28019d83
Junyuan Xue <junyuanxue.is@gmail.com>
2016-04-25 11:29:14 +0100

    moved angular js files to public folder

--- app/server.js
+++ app/server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile('index.html');
 });
 
 server.listen(3000, function() {
